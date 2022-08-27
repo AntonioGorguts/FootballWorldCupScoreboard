afd7115bb79694c903d574683cb2a4d92dfc9c97
unknown <anton_pasichenko@epam.com>
2022-08-28 01:45:36 +0300

    added football tournament tests

--- src/main/java/scoreboard/football/model/FootballTournament.java
+++ src/main/java/scoreboard/football/model/FootballTournament.java
@@ -3,10 +3,24 @@ package scoreboard.football.model;
 import scoreboard.common.model.tournament.TeamTournament;
 import scoreboard.common.model.tournament.Tournament;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
 public class FootballTournament implements TeamTournament<FootballTeam>, Tournament<FootballMatch> {
+
+    public FootballTournament(){
+
+    }
+
+    public FootballTournament(Comparator<FootballMatch> matchComparator) {
+
+    }
+
+    public Comparator<FootballMatch> getMatchComparator() {
+       return null;
+    }
+
     @Override
     public Set<FootballTeam> getActiveTeams() {
         return null;
---
+++ src/test/java/scoreboard/football/model/FootballTournamentTest.java
@@ -0,0 +1,47 @@
+package scoreboard.football.model;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class FootballTournamentTest {
+
+    @Test
+    public void shouldThrowExceptionWhenNullComparatorIsPassedToConstructor(){
+        //GIVEN WHEN
+        Comparator<FootballMatch> comparator = null;
+
+        //THEN
+        assertThrows(NullPointerException.class, () -> new FootballTournament(comparator));
+    }
+
+    @Test
+    public void shouldCreateTournamentWithComparator(){
+        //GIVEN WHEN
+        Comparator<FootballMatch> comparator = Comparator.comparing(FootballMatch::getScoreTotal);
+        FootballTournament footballTournament = new FootballTournament(comparator);
+
+        //THEN
+        assertEquals(footballTournament.getMatchComparator(), comparator);
+    }
+
+//    @Test
+//    public void shouldSortMatchesWithComparator(){
+//        //GIVEN WHEN
+//        Comparator<FootballMatch> comparator = Comparator.comparing(FootballMatch::getScoreTotal);
+//        FootballTournament footballTournament = new FootballTournament(comparator);
+//
+//        //THEN
+//    }
+//
+//    @Test
+//    public void shouldSortMatchesWithDefaultComparator(){
+//        //GIVEN WHEN
+//        FootballTournament footballTournament = new FootballTournament();
+//
+//        //THEN
+//    }
+}
