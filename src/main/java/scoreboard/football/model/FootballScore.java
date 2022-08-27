eac69f9958c0a3272ad63f8e300b68953da3d03f
unknown <anton_pasichenko@epam.com>
2022-08-28 01:13:51 +0300

    refactored football score to pass tests

--- src/main/java/scoreboard/football/model/FootballScore.java
+++ src/main/java/scoreboard/football/model/FootballScore.java
@@ -1,30 +1,38 @@
 package scoreboard.football.model;
 
 import scoreboard.common.model.score.TeamScore;
+import scoreboard.util.ErrorMessageUtil;
 
 public class FootballScore implements TeamScore {
     private int homeScore = 0;
     private int awayScore = 0;
+
     @Override
     public int getHomeScore() {
-        return 0;
+        return homeScore;
     }
 
     public void setHomeScore(int homeScore) {
+        if (homeScore < 0){
+            throw new IllegalArgumentException(ErrorMessageUtil.INVALID_HOME_SCORE);
+        }
         this.homeScore = homeScore;
     }
 
     @Override
     public int getAwayScore() {
-        return 0;
+        return awayScore;
     }
 
     public void setAwayScore(int awayScore) {
+        if (awayScore < 0){
+            throw new IllegalArgumentException(ErrorMessageUtil.INVALID_AWAY_SCORE);
+        }
         this.awayScore = awayScore;
     }
 
     @Override
     public Integer getScoreTotal() {
-        return null;
+        return homeScore + awayScore;
     }
 }
