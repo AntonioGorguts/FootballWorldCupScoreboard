ed76690bbbf1113bd3c39507a71d917df1a4e6b5
unknown <anton_pasichenko@epam.com>
2022-08-28 01:54:35 +0300

    refactored football tournament to pass test

--- src/main/java/scoreboard/football/model/FootballTournament.java
+++ src/main/java/scoreboard/football/model/FootballTournament.java
@@ -5,20 +5,23 @@ import scoreboard.common.model.tournament.Tournament;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class FootballTournament implements TeamTournament<FootballTeam>, Tournament<FootballMatch> {
+
+    private Comparator<FootballMatch> matchComparator;
 
     public FootballTournament(){
 
     }
 
     public FootballTournament(Comparator<FootballMatch> matchComparator) {
-
+        this.matchComparator = Objects.requireNonNull(matchComparator);
     }
 
     public Comparator<FootballMatch> getMatchComparator() {
-       return null;
+        return matchComparator;
     }
 
     @Override
