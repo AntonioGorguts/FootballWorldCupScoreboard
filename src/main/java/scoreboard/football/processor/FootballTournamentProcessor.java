b86f59475fdd94a5bd9528f1c53994fe43a46c10
unknown <anton_pasichenko@epam.com>
2022-08-28 17:04:42 +0300

    refactored tournament processor to pass parametrised tests

--- src/main/java/scoreboard/football/processor/FootballTournamentProcessor.java
+++ src/main/java/scoreboard/football/processor/FootballTournamentProcessor.java
@@ -1,12 +1,15 @@
 package scoreboard.football.processor;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import scoreboard.common.model.scoreboard.Scoreboard;
+import scoreboard.exception.MatchCommonException;
 import scoreboard.football.model.FootballMatch;
 import scoreboard.football.model.FootballScore;
 import scoreboard.common.processor.TeamProcessor;
 import scoreboard.common.processor.TournamentProcessor;
 import scoreboard.football.model.FootballTournament;
+import scoreboard.util.ErrorMessageUtil;
 
 import java.util.List;
 
@@ -23,19 +26,20 @@ public class FootballTournamentProcessor implements TournamentProcessor<Football
     public FootballTournament getFootballTournament() {
         return footballTournament;
     }
+
     @Override
     public void updateScore(FootballMatch footballMatch, FootballScore footballScore) {
-
+        matchCommonValidation(footballMatch);
     }
 
     @Override
     public void startMatch(FootballMatch footballMatch) {
-
+        matchCommonValidation(footballMatch);
     }
 
     @Override
     public void endMatch(FootballMatch footballMatch) {
-
+        matchCommonValidation(footballMatch);
     }
 
     @Override
@@ -46,5 +50,17 @@ public class FootballTournamentProcessor implements TournamentProcessor<Football
     @Override
     public List<String> getScoreboard() {
         return null;
+    }
+
+    private void matchCommonValidation(FootballMatch match){
+        if (match == null){
+            throw new MatchCommonException(ErrorMessageUtil.MATCH_NOT_NULL);
+        }
+        if (match.getHomeTeam() == null || StringUtils.isBlank(match.getHomeTeam().getName())){
+            throw new MatchCommonException(ErrorMessageUtil.HOME_TEAM_NOT_BLANK);
+        }
+        if (match.getAwayTeam() == null || StringUtils.isBlank(match.getAwayTeam().getName())){
+            throw new MatchCommonException(ErrorMessageUtil.AWAY_TEAM_NOT_BLANK);
+        }
     }
 }
